5150780440bd6cab6cc321d048fff47b2970d091
Jelmer Polman <mail@jelmerpolman.com>
2021-05-20 12:42:09 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './comps/**/*.{js,ts,jsx,tsx}'],
+    options: {
+      safelist: ['w-0', 'w-1/12', 'w-2/12', 'w-3/12', 'w-4/12', 'w-5/12', 'w-6/12', 'w-7/12', 'w-8/12', 'w-9/12', 'w-10/12', 'w-11/12', 'w-12/12'],
+    },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
